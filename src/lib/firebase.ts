a1c9452cf28f657c6591d08e80325d42e94dafc1
Valerie Pun <valeriepun1250-create@users.noreply.github.com>
2026-08-23 21:32:31 +0800

    Fix Firebase Pages deployment

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,17 +3,17 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "AIzaSyC6xM44RiCxw8E9c-mb79H3vy32WifqBzk",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "ota-app-cbdc2.firebaseapp.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyC6xM44RiCxw8E9c-mb79H3vy32WifqBzk",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "ota-app-cbdc2.firebaseapp.com",
   databaseURL:
-    process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ??
+    process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ||
     "https://ota-app-cbdc2-default-rtdb.firebaseio.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? "ota-app-cbdc2",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "ota-app-cbdc2",
   storageBucket:
-    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "ota-app-cbdc2.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "478224605139",
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "ota-app-cbdc2.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "478224605139",
   appId:
-    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ??
+    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
     "1:478224605139:web:d0916f056f4ae6e7fc1926",
 };
 
